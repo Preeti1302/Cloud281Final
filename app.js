f3afe557b2868b68ff200114006773e9a6911f25
tanvi6593 <tanvishukla234@gmail.com>
2016-05-25 11:21:00 -0700

    Node JS App 281

--- app.js
+++ app.js
@@ -43,139 +43,36 @@ var get_hash = function( state, ts ) {
 }
 
 
-var error = function( req, res, msg, ts ) {
 
-    body = fs.readFileSync('./gumball.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200);
+/*
+var writeToDB = function (req, res){
 
-    state = "error" ;
-    hash = get_hash ( state, ts ) ;
-
-    var html_body = "" + body ;
-    var html_body = html_body.replace("{message}", msg );
-    var html_body = html_body.replace("{ts}", ts );
-    var html_body = html_body.replace("{hash}", hash );
-    var html_body = html_body.replace(/id="state".*value=".*"/, "id=\"state\" value=\""+state+"\"") ;
-    res.end( html_body );
-}
+var fs = require("fs");
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://52.32.210.48:27017/write?w=0&readPreference=secondary";
 
 
 
-var page = function( req, res, state, ts ) {
+MongoClient.connect(url, function(err, db) {
+        var cursor =db.collection('hello').insert({
+  id:2,  print: "Hello World #1" });
+        var doc_total = {};
+        var index = 0;
+        cursor.each(function(err, doc) {
+          console.log("Doc is "+JSON.stringify(doc))
+          if (doc != null) {
+             doc_total[index++] = doc;
+          } else {
+              db.close();
+              res.end(JSON.stringify((doc_total),null,4));
+          }
+       });
+    });
+}       
 
-    body = fs.readFileSync('./gumball.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200);
-
-    hash = get_hash ( state, ts ) ;
-    console.log( state ) ;
-
-    var client = new Client();
-            var count = "";
-            client.get( endpoint, 
-                function(data, response_raw){
-                    console.log(data);
-                    count = data.countGumballs
-                    console.log( "count = " + count ) ;
-                    var msg =   "\n\nMighty Gumball, Inc.\n\nNodeJS-Enabled Standing Gumball\nModel# " + 
-                                data.modelNumber + "\n" +
-                                "Serial# " + data.serialNumber + "\n" +
-                                "\n" + state +"\n\n" ;
-                    var html_body = "" + body ;
-                    var html_body = html_body.replace("{message}", msg );
-                    var html_body = html_body.replace("{ts}", ts );
-                    var html_body = html_body.replace("{hash}", hash );
-                    var html_body = html_body.replace(/id="state".*value=".*"/, "id=\"state\" value=\""+state+"\"") ;
-                    res.end( html_body );
-            });
-}
-
-var order = function( req, res, state, ts ) {
-
-    var client = new Client();
-            var count = 0;
-            client.get( endpoint, 
-                function(data, response_raw) {
-                    count = data.countGumballs ;
-                    console.log( "count before = " + count ) ;
-                    if ( count > 0 ) {
-                        count-- ;
-                        var args = {
-                            data: {  "countGumballs": count, },
-                            headers:{"Content-Type": "application/json"} 
-                        };
-                        client.put( endpoint, args,
-                            function(data, response_raw) {
-                                console.log(data);
-                                console.log( "count after = " + data.countGumballs ) ;
-                                page( req, res, state, ts ) ;
-                            } 
-                        );
-                    }
-                    else {
-                        error( req, res, "*** OUT OF INVENTORY ***", ts ) ;
-                    }
-            });
-}
-
-
-var handle_post = function (req, res) {
-
-    console.log( "Post: " + "Action: " +  req.body.event + " State: " + req.body.state + "\n" ) ;
-    var hash1 = "" + req.body.hash ;
-    var state = "" + req.body.state ;
-    var action = "" + req.body.event ;
-    var ts = parseInt(req.body.ts) ;
-    var now = new Date().getTime() ;
-    var diff = ((now - ts)/1000) ;
-    hash2 = get_hash ( state, ts ) ;
-    console.log( "DIFF:  " +  diff ) ;
-    console.log( "HASH1: " + hash1 ) ;
-    console.log( "HASH2: " + hash2 ) ;
-
-    if ( diff > 120 || hash1 != hash2 ) {
-        error( req, res, "*** SESSION INVALID ***", ts ) ;
-    }
-    else if ( action == "Insert Quarter" ) {
-        if ( state == "no-coin" )
-            page( req, res, "has-coin", ts ) ;
-        else
-            page( req, res, state, ts ) ;
-            
-    }
-    else if ( action == "Turn Crank" ) {
-        if ( state == "has-coin" ) {
-            hash = get_hash ( "no-coin", ts ) ;
-            order(req, res, "no-coin", ts ) ;
-        }
-        else
-            page( req, res, state, ts ) ;
-    }  
-  
-}
-
-var handle_get = function (req, res) {
-    console.log( "Get: ..." ) ;
-
-    ts = new Date().getTime()
-    console.log( ts )
-    state = "no-coin" ;
-
-   // page( req, res, state, ts ) ;
-}
-
-var user = {
-   "user4" : {
-      "name" : "mohit",
-      "password" : "password4",
-      "profession" : "teacher",
-      "id": 4
-   }
-}
-
-
-var gett = function (req, res){
+*/
+var getfromDB = function (req, res){
 
 
 var fs = require("fs");
@@ -186,7 +83,7 @@ var url = "mongodb://52.32.210.48:27017,52.39.136.0:27017,52.39.27.47:27017/test
 
 
     MongoClient.connect(url, function(err, db) {
-        var cursor =db.collection('mycol').find();
+        var cursor =db.collection('hello').find();
         var doc_total = {};
         var index = 0;
         cursor.each(function(err, doc) {
@@ -205,9 +102,8 @@ var url = "mongodb://52.32.210.48:27017,52.39.136.0:27017,52.39.27.47:27017/test
 //app.set('port',5000);
 app.set('port', (process.env.PORT || 5000));
 
-app.post("*", handle_post );
-//app.get( "*", handle_get ) ;
-app.get("/test",gett)
+//app.post("/write",writeToDB);
+app.get("/",getfromDB );
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
